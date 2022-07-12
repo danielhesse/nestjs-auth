bc9812a1561f36c6b1bfa44df76cac464cd11a10
Daniel Hessel <daniel@sharbe.com.br>
2022-07-12 19:03:26 -0300

    feat(auth): implemente 'login' generating JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './models/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { Usertoken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  login(user: User): Usertoken {
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+    };
+
+    const jwt = this.jwtService.sign(payload);
+
+    return {
+      access_token: jwt,
+    };
+  }
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
